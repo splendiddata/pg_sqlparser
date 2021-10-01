68ed00db8ae4c0c3355c745037c6b045b2d4304e
mb <martin.butter@splendiddata.com>
2021-10-01 15:09:16 +0200

    Adapted PostgreSQL version 14.0

--- parser/src/main/java/com/splendiddata/sqlparser/ParserUtil.java
+++ parser/src/main/java/com/splendiddata/sqlparser/ParserUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Splendid Data Product Development B.V. 2020
+ * Copyright (c) Splendid Data Product Development B.V. 2020 - 2021
  *
  * This program is free software: You may redistribute and/or modify under the
  * terms of the GNU General Public License as published by the Free Software
@@ -310,12 +310,12 @@ public final class ParserUtil {
     }
 
     /**
-     * Returns parser version "13"
+     * Returns parser version "14"
      *
-     * @return String "13"
+     * @return String "14"
      */
     public static String getParserVersion() {
-        return "13";
+        return "14";
     }
 
     /**
@@ -324,7 +324,7 @@ public final class ParserUtil {
      * @return The major version of postgresql.
      */
     public static int getPostgresMajorVersion() {
-        return PostgresVersionMagicInteger.VERSION_THIRTEEN;
+        return PostgresVersionMagicInteger.VERSION_FOURTEEN;
     }
 
     /**
@@ -339,10 +339,10 @@ public final class ParserUtil {
     /**
      * Returns a String specifying for which Postgres version the parser is intended. In this version: "PostgreSQL 13".
      *
-     * @return String "PostgreSQL 13"
+     * @return String "PostgreSQL 14"
      */
     public static String getForPosgresVersion() {
-        return "PostgreSQL 13";
+        return "PostgreSQL 14";
     }
 
     /**
--- parser/src/main/java/com/splendiddata/sqlparser/PostgresVersionMagicInteger.java
+++ parser/src/main/java/com/splendiddata/sqlparser/PostgresVersionMagicInteger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Splendid Data Product Development B.V. 2020
+ * Copyright (c) Splendid Data Product Development B.V. 2020 - 2021
  *
  * This program is free software: You may redistribute and/or modify under the
  * terms of the GNU General Public License as published by the Free Software
@@ -65,5 +65,8 @@ public class PostgresVersionMagicInteger {
 
     /** Version number 13 */
     public static final int VERSION_THIRTEEN = 13;
+
+    /** Version number 13 */
+    public static final int VERSION_FOURTEEN = 14;
 
 }
